9e4a4acbbb38db13da8936f36580a5e1fdb53533
Nora Er-Rouhly <ner.tech21@gmail.com>
2023-12-19 20:37:26 +0100

    add docstrings

--- docker/llm/inference/chat_completion.py
+++ docker/llm/inference/chat_completion.py
@@ -9,22 +9,66 @@ api_key = os.environ.get('OPENAI_API_KEY')
 
 
 class Completion:
+    """
+    A class for interacting with the OpenAI GPT chat completions API.
+
+    Attributes:
+    - client: object
+              An instance of the OpenAI API client.
+    - model:  str
+              The GPT model identifier.
+    - temperature:  float
+                    The temperature parameter for controlling the randomness of responses.
+
+    Methods:
+    - __init__: Initializes the Completion object with the OpenAI API client, model, and temperature.
+    - verify_api_key: Static method to verify that the OpenAI API key is defined.
+    - verify_user_prompt: Static method to verify that the user prompt is defined.
+    - request_response: Sends a request to the OpenAI API for chat completion and prints the response.
+
+    Usage:
+    completion = Completion()
+    completion.request_response(user_prompt, system_prompt)
+    """
     def __init__(self):
+        """
+        Initializes the Completion object.
+
+        Attributes:
+        - client: object
+                An instance of the OpenAI API client.
+        - model:  str
+                The GPT model identifier.
+        - temperature:  float
+                        The temperature parameter for controlling the randomness of responses.
+        """
         self.client = openai.OpenAI(api_key=api_key)
         self.model = os.environ.get('model')
         self.temperature = float(os.environ.get('temperature'))
     
     @staticmethod
     def verify_api_key():
+        """
+        Static method to verify that the OpenAI API key is defined.
+        Raises AssertionError and exits the program if the key is not defined.
+        """
         try:
             assert api_key !=""
         except AssertionError:
-            print('OPENAI API KEY is empty. Please enter your API KEY in .env file')
+            print('OPENAI API KEY is not defined. Please enter your API KEY in .env file')
             sys.exit(1)
 
 
     @staticmethod
     def verify_user_prompt(user_prompt):
+        """
+        Static method to verify that the user prompt is not empty.
+        Raises AssertionError and exits the program if the prompt is empty.
+
+        Parameters:
+        - user_prompt: str
+                       The user prompt to be verified.
+        """
         try:
             assert user_prompt.strip() !=""
         except AssertionError:
@@ -32,6 +76,15 @@ class Completion:
             sys.exit(1)
 
     def request_response(self, user_prompt, system_prompt):
+        """
+        Sends a request to the OpenAI API for chat completion and prints the response.
+
+        Parameters:
+        - user_prompt: str
+                       The user's input prompt for the chat.
+        - system_prompt: str
+                         The system's input prompt for the chat.
+        """
         self.verify_api_key()
         self.verify_user_prompt(user_prompt)
 
